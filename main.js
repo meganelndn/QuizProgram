0f056d7b19c5673a1ec71fe8d2cf63f4149a136c
Mégane <megane.londono@hotmail.com>
2021-11-24 11:06:43 +0100

    todo and quiz done in same file

--- main.js
+++ main.js
@@ -1,3 +1,61 @@
+/******** TODO LIST ********/
+
+const addForm = document.querySelector(".add");
+const list = document.querySelector(".todos");
+const search = document.querySelector(".search input");
+
+const generateTemplate = todo => {
+    let html = `
+        <li class="list-group-item d-flex justify-content-between align-items-center">
+            <span>${todo}</span>
+            <i class="far fa-trash-alt delete"></i>
+        </li>
+    `;
+
+    list.innerHTML += html;
+}
+
+addForm.addEventListener("submit", e => {
+    e.preventDefault();
+
+    const todo = addForm.add.value.trim(); // trim() removes any spaces before/after input
+    
+    if(todo.length){
+        generateTemplate(todo);
+        addForm.reset() // resets input field, clears it
+    }
+})
+
+// delete todos
+list.addEventListener("click", e => {
+    if(e.target.classList.contains("delete")){
+        e.target.parentElement.remove();
+    }
+})
+
+// filter todos
+const filterTodos = term => {
+    // remove todos that do not match search query
+    Array.from(list.children)
+        .filter((todo) => !todo.textContent.toLowerCase().includes(term))
+        .forEach((todo) => todo.classList.add("filtered"))
+    
+    // keep todos that match search query
+    Array.from(list.children)
+        .filter((todo) => todo.textContent.toLowerCase().includes(term))
+        .forEach((todo) => todo.classList.remove("filtered"))
+}
+
+// keyup event
+search.addEventListener("keyup", () => {
+    const term = search.value.trim().toLowerCase();
+    filterTodos(term)
+})
+
+
+
+/******** QUIZ ********/
+
 const correctAnswers = ["D", "C", "D", "A"];
 const form = document.querySelector(".quiz-form");
 const result = document.querySelector(".result");
@@ -30,12 +88,3 @@ form.addEventListener("submit", e => {
         }
     }, 50);
 });
-
-// setInterval()
-/* let i = 0;
-const timer = setInterval(() => {
-    i++;
-    if(i === 5) {
-        clearInterval(timer);
-    }
-}, 1000); */
